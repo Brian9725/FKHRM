7ec843a9c1f737f475a95966f5ac61cf0c8c15fb
Brian <1786104060@qq.com>
2019-10-02 10:28:45 +0800

    添加EmployeeDao的关系映射

--- src/pers/brian/hrm/dao/EmployeeDao.java
+++ src/pers/brian/hrm/dao/EmployeeDao.java
@@ -5,13 +5,12 @@
  */
 package pers.brian.hrm.dao;
 
-import org.apache.ibatis.annotations.DeleteProvider;
-import org.apache.ibatis.annotations.InsertProvider;
-import org.apache.ibatis.annotations.SelectProvider;
-import org.apache.ibatis.annotations.UpdateProvider;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.mapping.FetchType;
 import pers.brian.hrm.dao.Provider.EmployeeDynaSQLProvider;
 import pers.brian.hrm.domain.Employee;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +21,18 @@ public interface EmployeeDao {
 
     // 根据参数动态查询员工
     @SelectProvider(type = EmployeeDynaSQLProvider.class, method = "selectByPage")
+    @Results({
+            @Result(id = true, column = "id", property = "id"),
+            @Result(column = "card_id", property = "cardId"),
+            @Result(column = "post_code", property = "postCode"),
+            @Result(column = "qq_num", property = "qqNum"),
+            @Result(column = "birthday", property = "birthday", javaType = Date.class),
+            @Result(column = "create_date", property = "createDate", javaType = Date.class),
+            @Result(column = "dept_id", property = "dept",
+                    one = @One(select = "pers.brian.hrm.dao.DeptDao.selectById", fetchType = FetchType.EAGER)),
+            @Result(column = "job_id", property = "job",
+                    one = @One(select = "pers.brian.hrm.dao.JobDao.selectById", fetchType = FetchType.EAGER))
+    })
     List<Employee> selectByPage(Map<String, Object> params);
 
     // 动态插入员工
@@ -30,10 +41,22 @@ public interface EmployeeDao {
 
     // 根据id删除员工
     @DeleteProvider(type = EmployeeDynaSQLProvider.class, method = "deleteById")
-   void deleteById(Integer id);
+    void deleteById(Integer id);
 
     // 根据id查询员工
     @SelectProvider(type = EmployeeDynaSQLProvider.class, method = "selectById")
+    @Results({
+            @Result(id = true, column = "id", property = "id"),
+            @Result(column = "card_id", property = "cardId"),
+            @Result(column = "post_code", property = "postCode"),
+            @Result(column = "qq_num", property = "qqNum"),
+            @Result(column = "birthday", property = "birthday", javaType = Date.class),
+            @Result(column = "create_date", property = "createDate", javaType = Date.class),
+            @Result(column = "dept_id", property = "dept",
+                    one = @One(select = "pers.brian.hrm.dao.DeptDao.selectById", fetchType = FetchType.EAGER)),
+            @Result(column = "job_id", property = "job",
+                    one = @One(select = "pers.brian.hrm.dao.JobDao.selectById", fetchType = FetchType.EAGER))
+    })
     Employee selectById(Integer id);
 
     // 动态修改员工
